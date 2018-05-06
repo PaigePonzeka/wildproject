2e3c0029d85177de1c0545c15c0999441bb8474c
Paige Ponzeka <pponzeka@yext.com>
2018-05-06 09:05:40 -0400

    Adding Styling to the Performance Archive page

--- wp-content/themes/wildproject/page-templates/page-archive-template.php
+++ wp-content/themes/wildproject/page-templates/page-archive-template.php
@@ -9,6 +9,7 @@ get_header(); ?>
 
   <div class="main-grid">
     <main class="main-content-full-width">
+
       <?php while ( have_posts() ) : the_post(); ?>
         <?php get_template_part( 'template-parts/content', 'page' ); ?>
       <?php endwhile; ?>
@@ -16,14 +17,17 @@ get_header(); ?>
         // Get all term ID's in a given taxonomy
         $taxonomy = 'performance_year';
         $taxonomy_terms = get_terms( $taxonomy, array(
-            'hide_empty' => 1
-        ) );
-
-        foreach($taxonomy_terms as $taxonomy_term):?>
-          <a href="<?php echo get_term_link($taxonomy_term); ?>">
-            <?php echo $taxonomy_term->name; ?>
-          </a>
+            'hide_empty' => 0
+        ) ); ?>
+        <div class="grid-x grid-margin-x archive-list">
+        <?php foreach($taxonomy_terms as $taxonomy_term):?>
+          <div class="cell medium-6 large-4">
+            <a href="<?php echo get_term_link($taxonomy_term); ?>">
+              <?php echo $taxonomy_term->name; ?>
+            </a>
+          </div>
         <?php endforeach;?>
+      </div>
     </main>
   </div>
 </div>
